d15f237248ccef048b47734f4920676d0cc38070
jaereeeee <jerelineannrosa@gmail.com>
2026-06-17 13:02:49 +0800

    first activity to commit :)

---
+++ basic_structure_and_Syntax/Main.java
@@ -0,0 +1,11 @@
+package basic_structure_and_Syntax;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("I'm Jereline Ann L. Rosa");
+        System.out.println("I'm 17 years old");
+        System.out.println("I live in Caloocan City");
+        System.out.println("I am a student of CZSHS and currently in Grade 12 section Peridot");
+    }
+}
